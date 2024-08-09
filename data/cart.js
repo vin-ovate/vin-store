690331b1a319504292645240a7cce0cccbf52fa7
vin-ovate <vinaypower69@gmail.com>
2024-08-09 19:58:10 +0530

    Update cart.js

--- data/cart.js
+++ data/cart.js
@@ -67,7 +67,7 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
     }
   });
 
-  matchingItem.deliveryOptionId = deliveryOptionId;
+  matchingItem.deliveryOptionId = deliveryOptionId ;
 
   saveToStorage();
 }
