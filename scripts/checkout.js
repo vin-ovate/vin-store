45a43ae0256e11586de262edb673e663ca7cb356
vin-ovate <vinaypower69@gmail.com>
2024-08-14 11:32:19 +0530

    Update checkout.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
-import {loadProducts, loadProductsFetch} from '../../data/products.js';
-import {loadCart} from '../../data/cart.js';
+import {loadProducts, loadProductsFetch} from './checkout/products.js';
+import {loadCart} from './checkout/cart.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
